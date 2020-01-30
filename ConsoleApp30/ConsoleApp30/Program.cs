d687460a1bdcb20b03b93f6ce99566ac5234715f
Pavel <pav199925@gmail.com>
2020-01-30 15:56:14 +0300

    Урок 5 (Полное 1 задание)
    Фуф, все понял. код в итоге лёгкий вышел. А времени много ушло

---
+++ ConsoleApp30/ConsoleApp30/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Введите логин");
+        string login = Console.ReadLine();
+        char[] arr = login.ToCharArray();
+
+        for (int i = 0; i < arr.Length; i++)
+            {
+            Console.Write(Char.IsNumber(arr[0]) ? "Первым символом не может быть цифра. " : "");
+                if (Char.IsNumber(arr[0])) {break;}
+            Console.Write(Char.IsLetterOrDigit(arr[i]) ? "В логине должны быть цифры или латинские буквы. " : "");
+                if (Char.IsLetterOrDigit(arr[i])) {break;}
+            }
+        if (arr.Length > 1 && arr.Length <= 10) { } else { Console.WriteLine(" Логин должен быть длинной от 2 до 10 символов"); }
+
+        Console.ReadKey();
+        }
+    }
+}
